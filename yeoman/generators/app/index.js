88ee3e53941215ff9385d9e6d3bc8bd0bdc46673
orb3bb <orb3bb@gmail.com>
2017-11-17 17:09:33 +0300

    Added solution copy and rename

--- yeoman/generators/app/index.js
+++ yeoman/generators/app/index.js
@@ -8,31 +8,25 @@ class MyGenerator extends Generator {
         super(args, opts);  
     }
 
-      // prompting() {
-      //   return this.prompt([{
-      //     type    : 'input',
-      //     name    : 'name',
-      //     message : 'Your project name',
-      //     default : this.appname // Default to current folder name
-      //   }, {
-      //     type    : 'confirm',
-      //     name    : 'cool',
-      //     message : 'Would you like to enable the Cool feature?'
-      //   }]).then((answers) => {
-      //     this.log('app name', answers.name);
-      //     this.log('cool feature', answers.cool);
-      //   });
-      // }
+      prompting() {
+        return this.prompt([{
+            type    : 'input',
+            name    : 'projectName',
+            message : 'Your project name',
+            default : 'Test'
+        }]).then((answers) => {
+            this._projectName = answers.projectName;
+        });
+    }
 
     writing() {
         this._copySolutionFile();
     }
 
-
     _copySolutionFile() {
         this.fs.copyTpl(
             this.templatePath('TemplateSolution.sln'),
-            this.destinationPath('TemplateSolution.sln')
+            this.destinationPath(`${this._projectName}.sln`)
         );
     }
 }
