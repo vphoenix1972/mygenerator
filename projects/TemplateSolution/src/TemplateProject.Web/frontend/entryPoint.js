5c2bcedb9924ded71f7c12356a1fcaf102e467bf
vphoenix1972 <vphoenix1972@gmail.com>
2018-08-02 10:08:21 +0300

    Removed webpack and frontend scripts

--- projects/TemplateSolution/src/TemplateProject.Web/frontend/entryPoint.js
+++
@@ -1,5 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/appModule';
-var platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
-//# sourceMappingURL=entryPoint.js.map
--- projects/TemplateSolution/src/TemplateProject.Web/frontend/entryPoint.ts
+++
@@ -1,5 +0,0 @@
-﻿import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/appModule';
-
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
