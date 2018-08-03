3a82a8d907911fa7aa0ddcfdb878f2e8b46f9366
vphoenix1972 <vphoenix1972@gmail.com>
2018-08-03 15:30:14 +0300

    Added ngBootstrap

--- projects/TemplateSolution/src/TemplateProject.Web/frontend/src/app/app.module.ts
+++ projects/TemplateSolution/src/TemplateProject.Web/frontend/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -18,6 +19,7 @@ import { TodoIndexComponent } from './todo-index/todo-index.component';
   ],
   imports: [
     BrowserModule,
+    NgbModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
