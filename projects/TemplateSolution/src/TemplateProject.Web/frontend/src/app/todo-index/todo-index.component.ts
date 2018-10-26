63b294d9c672e9816f63b7795beeb15bcaafdae1
vphoenix1972 <vphoenix1972@gmail.com>
2018-10-26 08:46:57 +0300

    Implemented todo-index.component and todo-items.service

--- projects/TemplateSolution/src/TemplateProject.Web/frontend/src/app/todo-index/todo-index.component.ts
+++ projects/TemplateSolution/src/TemplateProject.Web/frontend/src/app/todo-index/todo-index.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TodoItemsService } from 'src/app/services/todo-items.service';
+import { TodoItem } from '../models/todo-item';
+
 @Component({
-  selector: 'app-todo-index',
-  templateUrl: './todo-index.component.html',
-  styleUrls: ['./todo-index.component.scss']
+    selector: 'app-todo-index',
+    templateUrl: './todo-index.component.html',
+    styleUrls: ['./todo-index.component.scss']
 })
 export class TodoIndexComponent implements OnInit {
+    isLoading: boolean;
+    items: Array<any>;
 
-  constructor() { }
+    constructor(private _todoItemsService: TodoItemsService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.isLoading = true;
 
+        this._todoItemsService.getAll()
+            .subscribe(items => {
+                this.items = items
+
+                this.isLoading = false;
+            });
+    }
+
+    onAddButtonClicked(): boolean {
+        let newItem = new TodoItem();
+
+        newItem = this._todoItemsService.add(newItem);
+
+        this.items.push(newItem);
+
+        return false;
+    }
+
+    onEditButtonClicked(item: any): boolean {
+        console.log(`Edit item: ${item}`);
+
+        return false;
+    }
+
+    onDeleteButtonClicked(item: any): boolean {
+        this.items.splice(this.items.indexOf(item), 1);
+
+        this._todoItemsService.delete(item);
+
+        return false;
+    }
 }
