57aaeff95536ca552a032319eac0a85279c0dc20
vphoenix1972 <vphoenix1972@gmail.com>
2019-02-20 08:37:05 +0300

    Added SQLite support

--- yeoman/generators/app/templates/src/TemplateProject.DataAccess/ServiceCollectionInterfaceExtensions.DataAccess.cs
+++ yeoman/generators/app/templates/src/TemplateProject.DataAccess/ServiceCollectionInterfaceExtensions.DataAccess.cs
@@ -9,6 +9,19 @@ namespace <%= projectNamespace %>.DataAccess
         public static void AddDataAccess(this IServiceCollection services, string connectionString)
         {
             services.AddDbContext<ApplicationDbContext>(o => o.UseNpgsql(connectionString));
+
+            /*
+             * Uncomment the following line if you want to use SQLite instead of PostgreSQL.
+             * Don't forget to change connection string for SQLite in appsettings.json
+             * (example: "dbConnectionString": "Data Source=app.db").
+             *
+             * Note: SQLite support is not implemented in docker build, so if you want to use docker, choose PostgreSQL.
+             *
+            */
+
+            // services.AddDbContext<ApplicationDbContext>(o => o.UseSqlite(connectionString));
+
+
             services.AddScoped<IDatabaseService, DatabaseService>();
         }
     }
