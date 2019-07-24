604baca23d42e8efb1cf4ebca65602a4c45e0af0
vphoenix1972 <vphoenix1972@gmail.com>
2019-07-24 16:01:11 +0300

    Fixed IRepositoryBase

--- projects/TemplateSolution/src/TemplateProject.Core/Interfaces/DataAccess/Repositories/ITodoItemsRepository.cs
+++ projects/TemplateSolution/src/TemplateProject.Core/Interfaces/DataAccess/Repositories/ITodoItemsRepository.cs
@@ -1,8 +1,11 @@
-﻿using TemplateProject.Core.Domain;
+﻿using System.Collections.Generic;
+using TemplateProject.Core.Domain;
+using TemplateProject.Utils.Entities;
 
 namespace TemplateProject.Core.Interfaces.DataAccess.Repositories
 {
     public interface ITodoItemsRepository : IRepositoryBase<ITodoItem>
     {
+        (IList<ITodoItem> Items, int Total) GetMany(string nameFilter = null, int? limit = null, int? skip = null, string sortColumn = null, SortOrder? order = null);
     }
 }
